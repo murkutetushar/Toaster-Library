571ac5ce99ce8eee9dc547fb889fd735b00ad5fd
murkutetushar <tusharmurkute14@gmail.com>
2019-07-08 23:19:11 +0530

    Initial Commit

--- toasterlibrary/src/main/java/blackhat/com/mkstudio/team/toasterlibrary/Toaster.java
+++ toasterlibrary/src/main/java/blackhat/com/mkstudio/team/toasterlibrary/Toaster.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 
 public class Toaster {
 
-    public static void s(Context c, String message) {
+    public static void toaster(Context c, String message) {
 
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
 
