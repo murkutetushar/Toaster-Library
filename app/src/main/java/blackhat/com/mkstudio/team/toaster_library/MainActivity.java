d17d09cdc9f3f573d8f0ba4336317c73e4af623a
murkutetushar <tusharmurkute14@gmail.com>
2019-07-08 22:58:49 +0530

    Initial Commit

---
+++ app/src/main/java/blackhat/com/mkstudio/team/toaster_library/MainActivity.java
@@ -0,0 +1,17 @@
+package blackhat.com.mkstudio.team.toaster_library;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Tushar Murkute on 08/07/2019.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ toasterlibrary/src/main/java/blackhat/com/mkstudio/team/toasterlibrary/Toaster.java
@@ -0,0 +1,19 @@
+package blackhat.com.mkstudio.team.toasterlibrary;
+
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Tushar Murkute on 08/07/2019.
+ */
+
+public class Toaster {
+
+    public static void s(Context c, String message) {
+
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+
+
+    }
+}
